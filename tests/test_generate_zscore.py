83fd6f91cff6124970845c779624cf5653ca9791
cbemben <cbemben04@gmail.com>
2022-05-05 22:12:48 -0400

    adding zscore test

--- tests/test_generate_zscore.py
+++ tests/test_generate_zscore.py
@@ -1,16 +1,20 @@
 import os
+import pandas
 import pytest
-import freesolo.unsupervised_learning
+from freesolo import unsupervised_learning as us
 
 @pytest.fixture
 def raw_test_data():
-	test_df = pandas.DataFrame(columns=['count_example',
-										'z_score_example'],
-							   data = [])
+	data = pandas.DataFrame({'count_example': [4,5,3,7,5,12],
+								'z_score_example': [-0.6201736729, -0.3100868365,
+								                    -0.9302605094, 0.3100868365,
+								                    -0.3100868365, 1.860521019]})
 	yield data
-	os.remove(file_path)
 
 def test_for_zscore(raw_test_data):
-
-	usl = unsupervised_learning()
-	assert generate_standard_vars()
+	usl = us.UnsupervisedLearning(data=raw_test_data,
+		                        target_features=['count_example'])
+	usl.generate_standard_vars()
+	diff = usl._ads_data['count_example_zscore'] - raw_test_data['z_score_example']
+	print(usl._ads_data['count_example_zscore'])
+	assert sum(diff) == 0
